04e8a945e1d1da5052d91beb5e0146d85612085e
Your Name <you@example.com>
2024-10-06 11:16:51 +0330

    TsIgnores

--- src/components/ReviewItem.tsx
+++ src/components/ReviewItem.tsx
@@ -52,8 +52,8 @@ const ReviewItem: FC<ReviewItemProps> = ({
 
           <div className="mt-0.5 flex text-yellow-500">
             {
-              Array.from({ length: data.starPoint }).map(() =>   <StarIcon className="w-5 h-5" /> )
-            } 
+              Array.from({ length: data.starPoint }).map((_,index) =>   <StarIcon key={index} className="w-5 h-5" /> )
+            }
           </div>
         </div>
       </div>
