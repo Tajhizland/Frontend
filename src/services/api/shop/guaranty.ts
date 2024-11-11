f052ef33b800a6733ef7240dcb8d557553d79659
Your Name <you@example.com>
2024-11-11 17:22:02 +0330

    Guaranty

--- src/services/api/shop/guaranty.ts
+++ src/services/api/shop/guaranty.ts
@@ -2,7 +2,7 @@ import axios, {ServerResponse, SuccessResponseType} from "@/services/axios";
 import {GuarantyResponse} from "@/services/types/guaranty";
 
 export const findByUrl = async <T extends ServerResponse<GuarantyResponse>>
-(url
+(url:string
 ) => {
 
     return axios.post<T, SuccessResponseType<T>>("guaranty/find" , {url:url})
