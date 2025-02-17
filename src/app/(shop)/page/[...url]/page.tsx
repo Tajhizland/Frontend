cdd69dfbeda871f4cf5c520e52488123f4a1df59
Mehran <mehransobhani77@gmail.com>
2025-02-17 19:58:30 +0330

    FixAddress

--- src/app/(shop)/page/[...url]/page.tsx
+++ src/app/(shop)/page/[...url]/page.tsx
@@ -57,19 +57,12 @@ const BlogSingle = async (props: ProductPageProps) => {
             <>
 
             <header className="container rounded-xl mb-5">
-                <div className="max-w-screen-md mx-auto space-y-5 flex justify-between">
+                <div className="max-w-screen-md mx-auto space-y-5 flex justify-center">
                     <h1
                         className=" text-neutral-900 font-semibold text-3xl md:text-4xl md:!leading-[120%] lg:text-4xl dark:text-neutral-100 max-w-4xl "
-                        title="Quiet ingenuity: 120,000 lunches and counting"
                     >
                         {page.title}
                     </h1>
-                    <span
-                        style={{direction: "ltr"}}
-                        className=" text-neutral-500 font-semibold text-sm dark:text-neutral-100 "
-                    >
-            {page.created_at}
-          </span>
                 </div>
             </header>
             </>
@@ -91,7 +84,7 @@ const BlogSingle = async (props: ProductPageProps) => {
 
                 {renderHeader()}
                 <hr/>
-                <div className="nc-SingleContent container space-y-10 text-center page mt-5">
+                <div className="nc-SingleContent container space-y-10 text-right page mt-5">
                     {renderContent()}
                     <div
                         className="max-w-screen-md mx-auto border-b border-t border-neutral-100 dark:border-neutral-700"></div>
