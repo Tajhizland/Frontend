9ef0174f5cdf6fd3982ccb5b7a25531ede85f2a0
Mehran <mehransobhani77@gmail.com>
2025-03-10 20:59:57 +0330

    PwaNotification

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -39,7 +39,6 @@ export default function ShopLayout({
                     <BottomNavigation/>
                 </QueryClientProvider>
                 <Footer/>
-                <InstallPWAButton />
             </div>
             <CommonClient/>
         </>
