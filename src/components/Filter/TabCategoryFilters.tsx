0363b581e5139130ce220bd57c395108453c10aa
Mehran <mehransobhani77@gmail.com>
2025-04-21 19:44:16 +0330

    CategoryFilter

--- src/components/Filter/TabCategoryFilters.tsx
+++ src/components/Filter/TabCategoryFilters.tsx
@@ -21,6 +21,7 @@ import Radio from "@/shared/Radio/Radio";
 import Checkbox from "@/shared/Checkbox/Checkbox";
 import ButtonClose from "@/shared/Button/ButtonClose";
 import MySwitch from "@/shared/Switch/MySwitch";
+import {CategoryResponse} from "@/services/types/category";
 
 const DATA_sortOrderRadios = [
     {name: "جدید ترین", id: "Most-Popular"},
@@ -31,8 +32,9 @@ const DATA_sortOrderRadios = [
 ];
 type SelectedFilters = Record<string, string[] | string>;
 
-const TabFilters = ({filters, changeFilter, minPrice, maxPrice}: {
+const TabFilters = ({filters,categorys, changeFilter, minPrice, maxPrice}: {
     filters: FilterResponse[],
+    categorys: CategoryResponse[],
     changeFilter: (filters: string) => void,
     minPrice: number,
     maxPrice: number
@@ -327,6 +329,7 @@ const TabFilters = ({filters, changeFilter, minPrice, maxPrice}: {
                                             <ButtonThird
                                                 onClick={() => {
                                                     close();
+                                                    handleRemoveFilter(filter.id as string)
                                                     setColorsState([]);
                                                 }}
                                                 sizeClass="px-4 py-2 sm:px-5"
@@ -347,6 +350,89 @@ const TabFilters = ({filters, changeFilter, minPrice, maxPrice}: {
                     )}
                 </Popover>
             </Fragment>))
+
+        );
+    };
+    const renderCategorys = () => {
+        return (
+
+           <>
+               {categorys && categorys.length>0 &&
+                   <Popover className="relative" key={"category"}>
+                       {({open, close}) => (
+                           <>
+                               <PopoverButton
+                                   className={`flex items-center justify-center px-4 py-2 text-sm rounded-full border focus:outline-none select-none
+                ${open ? "!border-primary-500 " : ""}
+                  ${!!selectedFilters["category"]?.length
+                                       ? "!border-primary-500 bg-primary-50 text-primary-900"
+                                       : "border-neutral-300 dark:border-neutral-700 text-neutral-700 dark:text-neutral-300 hover:border-neutral-400 dark:hover:border-neutral-500"
+                                   }
+                  `}
+                               >
+                                   دسته بندی
+                                   {!selectedFilters["category"]?.length ? (
+                                       <ChevronDownIcon className="w-4 h-4 mr-3"/>
+                                   ) : (
+                                       <span
+                                           onClick={() => handleRemoveFilter("category")}>{renderXClear()}</span>
+                                   )}
+                               </PopoverButton>
+                               <Transition
+                                   as={Fragment}
+                                   enter="transition ease-out duration-200"
+                                   enterFrom="opacity-0 translate-y-1"
+                                   enterTo="opacity-100 translate-y-0"
+                                   leave="transition ease-in duration-150"
+                                   leaveFrom="opacity-100 translate-y-0"
+                                   leaveTo="opacity-0 translate-y-1"
+                               >
+                                   <PopoverPanel
+                                       className="absolute z-40 w-screen max-w-sm px-4 mt-3 right-0 sm:px-0 lg:max-w-sm">
+                                       <div
+                                           className="overflow-hidden rounded-2xl shadow-xl bg-white dark:bg-neutral-900 border border-neutral-200 dark:border-neutral-700">
+                                         <div className={"relative flex flex-col px-5 py-6 space-y-5"}>
+                                              {
+                                               categorys && categorys.map((item, index) => (
+                                                       <div key={index} className=" ">
+                                                           <Checkbox
+                                                               name={item.name}
+                                                               label={item.name}
+                                                               defaultChecked={selectedFilters["category"]?.includes(item.id + "")}
+                                                               onChange={(checked) => handleFilterChange("category", item.id + "", checked)}
+                                                           />
+                                                       </div>
+                                                   )
+                                               )
+                                           }
+
+                                         </div>
+                                           <div
+                                               className="p-5 bg-slate-50 dark:bg-slate-900 dark:border-t dark:border-slate-800 flex items-center justify-between">
+                                               <ButtonThird
+                                                   onClick={() => {
+                                                       close();
+                                                       handleRemoveFilter("category")
+                                                       setColorsState([]);
+                                                   }}
+                                                   sizeClass="px-4 py-2 sm:px-5"
+                                               >
+                                                   پاک کردن
+                                               </ButtonThird>
+                                               <ButtonPrimary
+                                                   onClick={close}
+                                                   sizeClass="px-4 py-2 sm:px-5"
+                                               >
+                                                   اعمال
+                                               </ButtonPrimary>
+                                           </div>
+                                       </div>
+                                   </PopoverPanel>
+                               </Transition>
+                           </>
+                       )}
+                   </Popover>}
+            </>
 
         );
     };
@@ -690,6 +776,24 @@ const TabFilters = ({filters, changeFilter, minPrice, maxPrice}: {
                                                 </Fragment>))
                                             }
 
+                                            {categorys && categorys.length>0 && <div className="py-7">
+                                                <h3 className="text-xl font-medium">دسته بندی</h3>
+                                                <div className="mt-6 relative ">
+                                                    {
+                                                        categorys && categorys.map((item, index) => (
+                                                                <div key={index} className="py-2">
+                                                                    <Checkbox
+                                                                        name={item.name}
+                                                                        label={item.name}
+                                                                        defaultChecked={selectedFilters["category"]?.includes(item.id + "")}
+                                                                        onChange={(checked) => handleFilterChange("category", item.id + "", checked)}
+                                                                    />
+                                                                </div>
+                                                            )
+                                                        )
+                                                    }
+                                                </div>
+                                            </div>}
                                             {/* --------- */}
                                             {/* ---- */}
 
@@ -845,6 +949,7 @@ const TabFilters = ({filters, changeFilter, minPrice, maxPrice}: {
             <div className="hidden lg:flex flex-1 gap-x-4">
                 {renderTabsPriceRage()}
                 {renderFilters()}
+                {renderCategorys()}
                 {renderTabIsOnsale()}
                 <div className="!mr-auto">{renderTabsSortOrder()}</div>
             </div>
