6d18b35b8b08cc17fd2d742aa93d37920a266651
Your Name <you@example.com>
2025-05-14 10:53:21 +0330

    Goftino

---
+++ public/js/goftino.js
@@ -0,0 +1,13 @@
+!function () {
+    var i = "DBwkk1", a = window, d = document;
+
+    function g() {
+        var g = d.createElement("script"), s = "https://www.goftino.com/widget/" + i,
+            l = localStorage.getItem("goftino_" + i);
+        g.async = !0, g.src = l ? s + "?o=" + l : s;
+        d.getElementsByTagName("head")[0].appendChild(g);
+    }
+
+    "complete" === d.readyState ? g() : a.attachEvent ? a.attachEvent("onload", g) : a.addEventListener("load", g, !1);
+}();
+
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,30 @@
 import localFont from 'next/font/local'
 import {NextFont} from "next/dist/compiled/@next/font";
 import "./globals.css";
+import Script from "next/script";
 
 
 const myFont: NextFont = localFont({src: '../fonts/fa/IRANSansWeb.woff2'})
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="fa" dir={"rtl"}>
-    <link rel="manifest" href="/manifest.json"/>
-    <meta name="theme-color" content="#fff"/>
-    <link rel="apple-touch-icon" href="/icons/logo.png"/>
-    <meta name="apple-mobile-web-app-status-bar" content="#fff"/>
-      <body
-        className={`${myFont.className}`}
-      >
+    return (
+        <html lang="fa" dir={"rtl"}>
+        <head>
+            {/*<Script src={"/js/goftino.js"}></Script>*/}
+            <link rel="manifest" href="/manifest.json"/>
+            <meta name="theme-color" content="#fff"/>
+            <link rel="apple-touch-icon" href="/icons/logo.png"/>
+            <meta name="apple-mobile-web-app-status-bar" content="#fff"/>
+        </head>
+        <body
+            className={`${myFont.className}`}
+        >
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
