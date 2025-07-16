8fbb734c937f984cb8a1042eab418d0a458f07c2
Your Name <you@example.com>
2025-07-16 14:43:51 +0330

    Pagination

---
+++ src/app/admin/user/address/[id]/page.tsx
@@ -0,0 +1,116 @@
+"use client"
+import {Fragment} from "react";
+import {useQuery} from "react-query";
+import {AddressResponse} from "@/services/types/address";
+import {Switch} from "@headlessui/react";
+import {getAddress} from "@/services/api/admin/user";
+import {useParams} from "next/navigation";
+
+const Page = () => {
+    const {id} = useParams();
+
+    const {data: address} = useQuery({
+        queryKey: ['my-address'],
+        queryFn: () => getAddress(Number(id)),
+        staleTime: 6000,
+    });
+    const renderItem = (item: AddressResponse) => {
+        return <div
+            className={"border rounded-lg flex flex-col p-5 text-neutral-500   dark:text-white dark:bg-black/20 text-sm md:text-base gap-y-10"}>
+            <div className={"flex items-center flex-auto flex-wrap gap-x-10 gap-y-5"}>
+                <div>
+                  <span className={"text-slate-500"}>
+                  عنوان :
+                  </span>
+                    <span className={"text-slate-600"}>
+                        {item.title}
+                    </span>
+                </div>
+                <div>
+                    <span className={"text-slate-500"}>
+                        استان :
+                    </span>
+                    <span className={"text-slate-600"}>
+                        {item.province?.name}
+                    </span>
+                </div>
+                <div>
+                    <span className={"text-slate-500"}>
+                        شهر :
+                    </span>
+                    <span className={"text-slate-600"}>
+                        {item.city?.name}
+                    </span>
+                </div>
+                <div>
+                    <span className={"text-slate-500"}>
+                        تلفن :
+                    </span>
+                    <span className={"text-slate-600"}>
+                        {item.tell}
+                    </span>
+                </div>
+                <div>
+                    <span className={"text-slate-500"}>
+                        موبایل :
+                    </span>
+                    <span className={"text-slate-600"}>
+                        {item.mobile}
+                    </span>
+
+                </div>
+                <div>
+                    <span className={"text-slate-500"}>
+                        کدپستی :
+                    </span>
+                    <span className={"text-slate-600"}>
+                        {item.zip_code}
+                    </span>
+                </div>
+            </div>
+
+            <div className={"flex  justify-center md:justify-start"}>
+                <p>
+                    {item.address}
+                </p>
+            </div>
+            <div className={"flex items-center gap-x-5 justify-between"}>
+                <div className={"flex items-center gap-x-5 justify-center  md:justify-start"}>
+                <span>
+                    فعال :
+                </span>
+                    <Switch
+
+                        disabled={item.active == 1 ? true : false}
+                        checked={item.active == 1 ? true : false}
+                        className={`${item.active ? "bg-teal-500" : "bg-slate-900"}
+          relative inline-flex h-[22px] w-[42px] shrink-0 cursor-pointer rounded-full border-4 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75 disabled:cursor-not-allowed`}
+                    >
+                        <span className="sr-only">فعال</span>
+                        <span
+                            aria-hidden="true"
+                            className={`${item.active ? "-translate-x-5" : "translate-x-0"}
+            pointer-events-none inline-block h-[14px] w-[14px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                        />
+                    </Switch>
+                </div>
+
+            </div>
+        </div>
+    }
+
+    return (
+        <>
+            <div className="space-y-10 sm:space-y-12  dark:text-white">
+
+                {
+                    address && address.map((item, index) => (<Fragment key={index}>
+                        {renderItem(item)}
+                    </Fragment>))
+                }
+            </div>
+        </>
+    );
+};
+
+export default Page;
--- src/services/api/admin/user.ts
+++ src/services/api/admin/user.ts
@@ -1,27 +1,52 @@
 import axios, {ServerResponse, SuccessResponseType} from "@/services/axios";
 import {UserResponse} from "@/services/types/user";
-
+import {OnHoldOrderResponse} from "@/services/types/onHoldOrder";
+import {OrderResponse} from "@/services/types/order";
+import {AddressResponse} from "@/services/types/address";
 
 
 export const update = async <T extends ServerResponse<unknown>>
 (
     params: {
-        id:number ;
-        name:string ;
-        username:string ;
-        email:string ;
-        gender:string ;
-        role:string ;
+        id: number;
+        name: string;
+        username: string;
+        email: string;
+        gender: string;
+        role: string;
     }
 ) => {
-    return axios.post<T, SuccessResponseType<T>>("admin/user/update" , params)
+    return axios.post<T, SuccessResponseType<T>>("admin/user/update", params)
         .then((res) => res?.data)
 };
 
 export const findById = async <T extends ServerResponse<UserResponse>>
 (
-    id:number|string
+    id: number | string
 ) => {
-    return axios.get<T, SuccessResponseType<T>>("admin/user/find/"+id )
+    return axios.get<T, SuccessResponseType<T>>("admin/user/find/" + id)
         .then((res) => res?.data?.result?.data)
+};
+export const getAddress = async <T extends ServerResponse<AddressResponse[]>>
+(
+    id: number | string
+) => {
+    return axios.get<T, SuccessResponseType<T>>("admin/user/address/" + id)
+        .then((res) => res?.data?.result?.data)
+};
+export const getOrder = async <T extends ServerResponse<OrderResponse[]>>
+(
+    id: number | string,
+    page: number
+) => {
+    return axios.get<T, SuccessResponseType<T>>("admin/user/order/" + id + "?page=" + page)
+        .then((res) => res?.data?.result)
+};
+export const getOnHoldOrder = async <T extends ServerResponse<OnHoldOrderResponse[]>>
+(
+    id: number | string,
+    page: number
+) => {
+    return axios.get<T, SuccessResponseType<T>>("admin/user/find/" + id + "?page=" + page)
+        .then((res) => res?.data?.result)
 };
