12437dff08d4b8ce7472b0f413c4671af831cf46
Mehran <mehransobhani77@gmail.com>
2025-10-13 07:29:30 +0330

    fixBug

--- src/middleware.ts
+++ src/middleware.ts
@@ -34,8 +34,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-    const token = request.cookies.get('token')?.value;
-    return NextResponse.next();
+    const token = request.cookies.get('token')?.value; 
 
     try {
         const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_API_URL + "auth/me", {
