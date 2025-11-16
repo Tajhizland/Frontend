a88122d050468875b0de589407cc4de6060c6900
Mehran <mehransobhani77@gmail.com>
2025-11-16 08:16:20 +0330

    Banner

--- src/app/admin/campaign/Form.tsx
+++ src/app/admin/campaign/Form.tsx
@@ -35,8 +35,8 @@ export default function Form({data, submit, loading = false}: Form) {
             setValue("title", data.title);
             setValue("color", data.color);
             setValue("status", data.status.toString());
-            setValue("start_date", data.start_date);
-            setValue("end_date", data.end_date);
+            setValue("start_date", data.start_date_fa);
+            setValue("end_date", data.end_date_fa);
             setValue("banner", data.banner);
             setValue("logo", data.logo);
         }
